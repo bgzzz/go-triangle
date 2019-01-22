35aacb0e24a1aaa1ffa11daab81d65c6c764b5d3
bgzzz <bgzzz>
2019-01-22 17:10:24 +0300

    test(utility): add parsing tests

---
+++ utility_test.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestParseArgError(t *testing.T) {
+	inputArgs := []string{"12,", "10 ", "242.34sdfsdf"}
+
+	_, err := parseArgs(inputArgs)
+
+	if err != nil {
+		t.Log(err.Error())
+	} else {
+		t.Error("It should be an error")
+	}
+
+}
+
+func TestParseArg(t *testing.T) {
+	inputArgs := []string{"12,", "10 ", "242.34	,", "242.22	;"}
+	expectedArray := []float64{12, 10, 242.34, 242.22}
+
+	parsedArray, err := parseArgs(inputArgs)
+
+	if err != nil {
+		t.Error(err.Error())
+	}
+
+	if !reflect.DeepEqual(parsedArray, expectedArray) {
+		t.Errorf("Parsed array != expected (%+v %+v)",
+			parsedArray, expectedArray)
+	}
+
+}
